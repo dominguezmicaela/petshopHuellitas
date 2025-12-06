aebd76b33fac337ef1e7f0b30ff77b700f247b7e
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-12-06 19:59:12 -0300

    implementacion de las funciones del carrito

--- js/funcionesCarrito.js
+++ js/funcionesCarrito.js
@@ -1,4 +1,25 @@
 import { guardarCarrito,obtenerCarrito,vaciarCarrito } from "./storage.js";
 import {actualizarContador,mostrarMensaje} from "./ui.js";
+//agregarr
+export const agregarAlCarrito=(producto)=>{
+    const carrito= obtenerCarrito();
+    carrito.push(producto); //insrta producto
+    guardarCarrito(carrito); //actualiza la info en el local storage
+    actualizarContador(carrito);
+    mostrarMensaje("Producto agregado");
+} ;
+//eliminar
+export const eliminarProducto=(indice)=>{
+    const carrito= obtenerCarrito()
+    carrito.splice(indice,1)//modifica  array en el tercer argumento seria para reemplazar pero como no pase nada entonces lo elimina.
+    guardarCarrito(carrito);
+    actualizarContador(carrito);
+    mostrarMensaje("Producto eliminado");
+};
 
-export const agregarAlCarrito
+export const vaciarCarrito=()=>{
+    vaciarCarrito();
+    actualizarContador([]);
+    mostrarMensaje("Carrito vaciado");
+};
+
