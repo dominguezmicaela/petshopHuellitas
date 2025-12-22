58047c9337dbd9adb7398a661a767eb7346e3114
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-12-22 06:35:38 -0300

    ajustes finales

--- huellitasBackEnd/Huellitas.API/Program.cs
+++ huellitasBackEnd/Huellitas.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
    options.AddPolicy("PermitirFrontend",
         policy =>
         {
-            policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
