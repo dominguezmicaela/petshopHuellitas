a3893ac5a8c88c53c4353e22fe40f0be55441af4
Micaela Dominguez <dominguezmicaelabelen35@gmail.com>
2025-12-26 09:16:44 -0300

    feat: agregar interfaz IProductoRepositorio

---
+++ huellitasBackEnd/Huellitas.Core/Interfaces/IProductoRepositorio.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace Huellitas.Core.Interfaces
+{
+    public interface IProductoRepositorio
+    {
+        Task<IEnumerable<Producto>> ObtenerTodosAsync();
+        Task<Producto?> ObtenerPorIdAsync(int id);
+        Task<Producto> CrearAsync(Producto producto);
+    }
+}
