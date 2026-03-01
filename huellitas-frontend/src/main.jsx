71b296c7a6cb7cd7bdb11fc29563a67cc6e0a9f0
Micaela Dominguez <dominguezmicaelabelen35@gmail.com>
2026-03-01 16:47:03 -0300

    fix: HashRouter para GitHub Pages

--- huellitas-frontend/src/main.jsx
+++ huellitas-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter } from "react-router-dom";
 import App from "./App";
 import { CarritoProvider } from "./context/CarritoContext";
 import "./index.css";
